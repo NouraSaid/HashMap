80bfdd1625aa56725af58e61e5e8cfaf1cd85f88
NouraSaid <noura.alsaidi98@gmail.com>
2024-03-04 11:48:08 +0400

    SizeHashMap

---
+++ SizeHashMap.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+
+class SizeHashMap {
+    public static void main(String[] args) {
+
+        // create a new hashmap
+        HashMap<String, Integer> results = new HashMap<>();
+
+        System.out.println("A HashMap for mapping marks to students");
+
+        // check if the results hashmap is empty
+        boolean empty = results.isEmpty();
+
+
+        // add elements to the hashmap
+
+        results.put("Noura", 100);
+        results.put("AFNAN", 100);
+
+        //display the hashmap
+
+        System.out.println("HashMap Size : " + results.size());
+
+    }
+}
